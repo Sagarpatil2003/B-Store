a2c07d864451b546f88a0e407eac8e2ed94e4a58
Sagarpatil2003 <sp4765699@gmail.com>
2024-09-09 10:52:14 +0530

    Crud  test case

--- B-Store_Back-end/blog/tests.py
+++ B-Store_Back-end/blog/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Book
+from django.utils import timezone
 
-# Create your tests here.
+
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Sample Book",
+            author="John Doe",
+            description="A sample book description.",
+            published_date=timezone.now(),
+            price= 100.00
+        )
+        
+    def book_creation_test(self):
+        self.assertEqual(self.book.title,"Sample Book")
+        self.assertEqual(self.book.author,"John Doe")
+        self.assertTrue(self.book.created_at)
+    
+    def test_book_string_representation(self):
+        self.assertEqual(str(self.book),self.book.title)
+    
+    def  test_book_clean(self):
+        with self.assertRaisesMessage(ValueError, "The title field cannot be empty."):
+            self.book.title=''
+            self.book.full_clean()
+
+
